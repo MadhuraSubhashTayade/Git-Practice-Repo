723e0d0bc7b00defce278662e2f4b6c33e87887d
Madhura Subhash Tayade <tayademadhura02@gmail.com>
2022-10-03 16:36:23 +0530

    added multiply.js and modified text with mul code

--- text.js
+++ text.js
@@ -4,3 +4,9 @@ const sum = () => {
   return a + b;
 };
 console.log("Sum is " + sum());
+
+// this is for subdev branch
+const multiply = (n1, n2) => {
+  return n1 * n2;
+};
+console.log("Multiplication: ", multiply(20, 4));
