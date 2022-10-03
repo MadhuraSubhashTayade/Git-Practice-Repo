8623a898c7f7289c5ac8d786fce0344321749852
Madhura Subhash Tayade <tayademadhura02@gmail.com>
2022-10-03 15:52:21 +0530

    Changes in javascript code

--- text.js
+++ text.js
@@ -1,6 +1,11 @@
 const a = 10;
 const b = 20;
-const sum = () => {
+const add = () => {
   return a + b;
 };
-console.log("Sum is " + sum());
+const sum = add();
+console.log("Sum is " + sum);
+
+document.querySelector(".num1").textContent = a;
+document.querySelector(".num2").textContent = b;
+document.querySelector(".sum").textContent = sum;
