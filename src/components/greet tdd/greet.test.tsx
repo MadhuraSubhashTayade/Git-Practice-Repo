f09cc0ed24c03f28bb1deda42d0f771d8af4efbc
Madhura Subhash Tayade <tayademadhura02@gmail.com>
2022-10-13 16:48:20 +0530

    another commit maybe 3

--- src/components/greet tdd/greet.test.tsx
+++ src/components/greet tdd/greet.test.tsx
@@ -1,14 +1,19 @@
 import { render, screen } from "@testing-library/react";
 import Greet from "./greet";
 
-test("Greet renders", () => {
-  render(<Greet />);
-  const ele = screen.getByText(/hello/i);
-  expect(ele).toBeInTheDocument();
-});
+describe("Greet group", () => {
+  beforeEach(() => {
+    console.log("before done");
+  });
+  test("Greet renders", () => {
+    render(<Greet />);
+    const ele = screen.getByText(/hello/i);
+    expect(ele).toBeInTheDocument();
+  });
 
-test("Greet rendres with message", () => {
-  render(<Greet name="Bruce" />);
-  const ele = screen.getByText(/hello bruce/i);
-  expect(ele).toBeInTheDocument();
+  test("Greet renders with message", () => {
+    render(<Greet name="Bruce" />);
+    const ele = screen.getByText(/hello brUCe/i);
+    expect(ele).toBeInTheDocument();
+  });
 });
