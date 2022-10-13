9fd49d4c15087edcf40543c624c6d32ee2591cd0
Madhura Subhash Tayade <tayademadhura02@gmail.com>
2022-10-13 17:56:13 +0530

    third commit 4

--- src/components/demo/demo.test.tsx
+++ src/components/demo/demo.test.tsx
@@ -1,13 +1,26 @@
+import { render, screen } from "@testing-library/react";
 describe("Group 1", () => {
-  beforeAll(() => {
-    console.log("before AbeforeAll called...");
+  beforeEach(() => {
+    console.log("before each1 called...");
   });
 
-  beforeEach(() => {
-    console.log("before each called...");
+  beforeAll(() => {
+    console.log("before All called...");
   });
 
   test("expect sum to be 4", () => {
     expect(2 + 2).toBe(4);
+  });
+
+  test("expect sum to be 40", () => {
+    expect(20 + 20).toBe(40);
+  });
+
+  afterEach(() => {
+    console.log("after each called...");
+  });
+
+  afterAll(() => {
+    console.log("after All called...");
   });
 });
